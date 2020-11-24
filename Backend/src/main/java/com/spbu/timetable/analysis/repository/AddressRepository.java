fba9c970b5efd390c8e618657cb976944febbddd
Anastasia <Anastasia_Berlina@epam.com>
2020-11-24 22:22:43 +0300

    fix repo naming

--- Backend/src/main/java/com/spbu/timetable/analysis/repository/AddressRepository.java
+++ Backend/src/main/java/com/spbu/timetable/analysis/repository/AddressRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,6 +21,6 @@ public interface AddressRepository extends JpaRepository<Address, UUID> {
             " WHERE adr.buildingName LIKE CONCAT('%',:search,'%') " +
             " or adr.street LIKE CONCAT('%',:search,'%')" +
             " and adr.GCRecord = null")
-    Page<Address> findAllByGCRecordIsNotNullAndStreetLike(Pageable pageable, String search);
+    Page<Address> findAllByGCRecordIsNotNullAndStreetLike(Pageable pageable, @Param("search") String search);
 
 }
